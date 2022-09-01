43ba67594608de242cb14ace13ccb23d986d85af
Jaime <jaimehiguerasmedina98@gmail.com>
2022-09-01 12:11:23 +0200

    Añadidas dependencias para clientes de Eureka

--- discovery-server/src/main/java/com/discoveryserver/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/discoveryserver/discoveryserver/DiscoveryServerApplication.java
@@ -2,12 +2,17 @@ package com.discoveryserver.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+
 	}
 
 }
